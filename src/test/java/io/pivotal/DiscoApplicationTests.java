d2abfbd8ac4c1734a3aced4c1139f07761290c72
Augustus Lidaka, Greg Chattin-McNichols, Kurtis Seebaldt and Sarah McAlear <pair+alidaka+gchattinmcnichols+kseebaldt+smcalear@pivotal.io>
2016-02-09 15:24:14 -0800

    fix test bootstrap circular dependency [#113407573]

--- src/test/java/io/pivotal/DiscoApplicationTests.java
+++ src/test/java/io/pivotal/DiscoApplicationTests.java
@@ -3,10 +3,12 @@ package io.pivotal;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = DiscoApplication.class)
+@WebIntegrationTest("server.port=0")
 public class DiscoApplicationTests {
 
 	@Test
